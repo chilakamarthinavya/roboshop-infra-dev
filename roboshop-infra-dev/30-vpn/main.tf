4a53c9579eb8af6762f1f8843ac4bb02337129fe
chilakamarthinavya <chilakamarthinavya@gmail.com>
2025-07-05 23:36:11 +0530

    terraform

---
+++ roboshop-infra-dev/30-vpn/main.tf
@@ -0,0 +1,22 @@
+ /*  resource "aws_key_pair" "openvpn" {
+  key_name   = "openvpn"
+  public_key = file("C:\\devops\\repos\\openvpn.pub")  # no need to import if key already present 
+}  
+ */
+
+
+ resource "aws_instance" "vpn" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.vpn_sg_id]
+  subnet_id = local.public_subnet_id
+  key_name= "repos" #make sure this key exists in AWS 
+  #key_name= aws_key_pair.openvpn.key_name
+  user_data= file("openvpn.sh")
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-vpn"
+    }
+  )
+} 
