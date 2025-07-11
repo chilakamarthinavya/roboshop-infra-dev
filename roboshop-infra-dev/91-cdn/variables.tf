783c33c3a2c202912297962e86dc55e0c534254b
chilakamarthinavya <chilakamarthinavya@gmail.com>
2025-07-11 16:17:25 +0530

    terraform

---
+++ roboshop-infra-dev/91-cdn/variables.tf
@@ -0,0 +1,16 @@
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z032558618100M4EJX8X4"
+}
+
+variable "zone_name" {
+    default = "daws84s.site"
+}
